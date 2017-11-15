f63a53ddcdd16b73cd10c010d07e66f7857224d8
Mario Harper <harpmari12@gmail.com>
2017-11-14 20:14:31 -0600

    phase-3

--- galleryService.js
+++ galleryService.js
@@ -1,54 +1,16 @@
+const contentful = require('contentful');
+
 module.exports = {
   getGallery
 };
 
+const client = contentful.createClient({
+  space: 'rprfeafqwgam',
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN, // your contentful access token
+});
+
 function getGallery() {
-  return Promise.resolve([
-    {
-      url: `assets/images/natgeo1.jpg`,
-      alt: `image 1 alt`
-    }, {
-      url: `assets/images/natgeo2.jpg`,
-      alt: `image 2 alt`
-    },    {
-      url: `assets/images/natgeo3.jpg`,
-      alt: `image 3 alt`
-    }, {
-      url: `assets/images/natgeo4.jpg`,
-      alt: `image 4 alt`
-    },    {
-      url: `assets/images/natgeo5.jpg`,
-      alt: `image 5 alt`
-    }, {
-      url: `assets/images/natgeo6.jpg`,
-      alt: `image 6 alt`
-    },    {
-      url: `assets/images/natgeo7.jpg`,
-      alt: `image 7 alt`
-    }, {
-      url: `assets/images/natgeo8.jpg`,
-      alt: `image 8 alt`
-    },    {
-      url: `assets/images/natgeo9.jpg`,
-      alt: `image 9 alt`
-    }, {
-      url: `assets/images/natgeo10.jpg`,
-      alt: `image 10 alt`
-    },    {
-      url: `assets/images/natgeo11.jpg`,
-      alt: `image 11 alt`
-    }, {
-      url: `assets/images/natgeo12.jpg`,
-      alt: `image 12 alt`
-    },    {
-      url: `assets/images/natgeo13.jpg`,
-      alt: `image 13 alt`
-    }, {
-      url: `assets/images/natgeo14.jpg`,
-      alt: `image 14 alt`
-    },   {
-      url: `assets/images/natgeo15.jpg`,
-      alt: `image 15 alt`
-    }
-  ]);
+  return client.getEntries({
+    'content_type': '7leLzv8hW06amGmke86y8G', // content type for Photo Gallery
+  }).then((entries) => entries.items[0].fields.images); // let error throw if not defined
 }
